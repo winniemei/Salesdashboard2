03f8abad7e5c5501d0e6190a481d44956c2502e6
winniemei <wmei91@gmail.com>
2023-08-10 15:50:22 -0400

    dashboard template

---
+++ my-dashboard/src/components/Transactions.jsx
@@ -0,0 +1,12 @@
+import {mockTransactions} from "../mockData";
+
+
+export default function Transactions () {
+    const allTransactions = mockTransactions.map((ele) => 
+    {ele.user})
+    return(
+        <div>
+            <p>{console.log({allTransactions})}</p>
+        </div>
+    )
+}
